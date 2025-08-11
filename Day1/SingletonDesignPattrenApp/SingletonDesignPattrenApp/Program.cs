1f82407a208497a02df61968ad9d6399079d91b5
kannan <kannans89@hotmail.com>
2025-08-11 12:42:22 +0530

    singleton

---
+++ Day1/SingletonDesignPattrenApp/SingletonDesignPattrenApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using SingletonDesignPattrenApp;
+
+var regstry1 =  ServiceRegistry.CreateInstance();
+var regstry2 = ServiceRegistry.CreateInstance();
+
+Console.WriteLine(regstry1.GetHashCode());
+Console.WriteLine(regstry2.GetHashCode());
+
+regstry1.DoWork();
+regstry2.DoWork();
+
---
+++ Day1/SingletonDesignPattrenApp/SingletonDesignPattrenApp/ServiceRegistry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattrenApp
+{
+    internal class ServiceRegistry
+    {
+        private  static ServiceRegistry _bucket;
+        private ServiceRegistry() {
+
+            Console.WriteLine("service registry created");
+        }
+
+        public static ServiceRegistry CreateInstance() { 
+        
+            if(_bucket == null) {
+               _bucket = new ServiceRegistry();
+            }
+
+            return _bucket;
+        }
+
+        //instance method
+        public void DoWork() {
+            Console.WriteLine("Service registry id doing some work..");
+        }
+    }
+}

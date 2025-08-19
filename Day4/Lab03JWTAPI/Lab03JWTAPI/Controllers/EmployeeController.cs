9fed4f0958be2f7f01f06de2f185eaae3b2bb744
kannan <kannans89@hotmail.com>
2025-08-19 14:17:08 +0530

    jwt final

---
+++ Day4/Lab03JWTAPI/Lab03JWTAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Lab03JWTAPI.Data;
+using Lab03JWTAPI.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab03JWTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        private ApplicationDbContext _dbcontext;
+
+        public EmployeeController(ApplicationDbContext dbcontext)
+        { 
+         _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(_dbcontext.Employees.ToList());
+        }
+
+        [HttpPost]
+        [Authorize(Roles ="admin")]
+        public IActionResult CreateEmployee(Employee emp) { 
+        
+            _dbcontext.Employees.Add(emp);
+            _dbcontext.SaveChanges();
+
+            return Created();
+        
+        }
+
+    }
+}
